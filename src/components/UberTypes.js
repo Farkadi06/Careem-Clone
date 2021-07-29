809803be5263cd81980d6537bc94b186116ded54
FARKADI OUSSAMA <58217000+Farkadi06@users.noreply.github.com>
2021-07-29 15:17:05 +0100

    Used a Bottom sheet instead of a simple component

--- src/components/UberTypes.js
+++ src/components/UberTypes.js
@@ -1,16 +1,38 @@
-import React from 'react'
-import { StyleSheet, Text, View, Pressable } from 'react-native'
+import React, { useRef } from 'react'
+import { StyleSheet, Text, View, Pressable  } from 'react-native'
 import UberTypesRow from './UberTypesRow'
 import typesData from '../types'
+import RBSheet from "react-native-raw-bottom-sheet";
+import { Feather } from '@expo/vector-icons';
 
 const UberTypes = () => {
-
+    const refRBSheet = useRef();
     const confirm = () => {
-        console.log('confirm')
+        //qx
     }
 
     return (
         <View >
+        <RBSheet
+            ref={refRBSheet}
+            closeOnDragDown={true}
+            closeOnPressMask={false}
+            dragFromTopOnly={true}
+            closeOnPressMask={true}
+            height = {350}
+            customStyles={{
+              container: {
+                  backgroundColor: "#fff"
+              },
+              wrapper: {
+                backgroundColor: "transparent"
+              },
+              draggableIcon: {
+                backgroundColor: "#000"
+              }
+            }}
+            animationType = {"fade"}
+        >
             {typesData?.map(type => <UberTypesRow
                 key ={type.id}
                 UberImage = {type.type}
@@ -23,6 +45,11 @@ const UberTypes = () => {
                 <Text style={{color: 'white', textAlign: 'center', fontWeight: 'bold'}}>
                     Confirm Uber
                 </Text>
+            </Pressable>
+
+        </RBSheet>
+            <Pressable onPress={() => refRBSheet.current.open()} style={styles.SwipBtn}>
+                <Feather name={'menu'} size={44}  color="black" />
             </Pressable>
         </View>
 
@@ -39,6 +66,19 @@ const styles = StyleSheet.create({
         padding: 10,
         margin:15,
         borderRadius:5
+    },
+    SwipBtn: {
+        margin: -32,
+        alignSelf:"center",
+        backgroundColor: "lightgrey",
+        width: 50,
+        height: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 25,
+        borderColor: "lightgrey",
+        borderWidth: 2,
     }
+    
 
 })
